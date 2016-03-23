751d8d0977e9b5736f9c7bbe97451cc804c7ebcb
Nasy <sy_n@me.com>
2016-03-23 19:53:43 +0800

    update 0401.py
    use numpy array instead of list

--- 0401.py
+++ 0401.py
@@ -10,20 +10,20 @@ import pylab
 
 x = pylab.np.linspace(20, 40, 5)
 
-y = [20, 28, 40.5, 52.5, 72]
+y = pylab.np.array([20, 28, 40.5, 52.5, 72])
 
 # y1 = [2, 3, 4.5, 5.5, 8]
 
 # y2 = [2, 3, 4.5, 5.5, 8]
 
-y1 = [18, 25, 36, 47, 64]
+y1 = pylab.np.array([18, 25, 36, 47, 64])
 
-y2 = [22, 31, 45, 58, 80]
+y2 = pylab.np.array([22, 31, 45, 58, 80])
 
-for i in range(5):
-    y1[i] = y[i] - y1[i]
-    y2[i] = y2[i] - y[i]
+# for i in range(5):
+#     y3[i] = y[i] - y1[i]
+#     y4[i] = y2[i] - y[i]
 
-pylab.errorbar(x, y, [y1, y2],ecolor='red',color='black')
+pylab.errorbar(x, y, [y - y1, y2 - y],ecolor='red',color='black')
 pylab.xlim([15, 45])
 pylab.show()
